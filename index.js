957dddb736d8fc7c68d7220c3467ade1bffd8b8a
jimmy <jmetz2510@gmail.com>
2023-06-09 22:30:11 -0400

    Remove bug from non-number/letter keys

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function isNumberKey(evt) {
     if (charCode != 46 && charCode > 31
         && (charCode < 47 || charCode > 57))
         return false;
+    if (charCode < 47) {
+        return false;
+    }
     console.log(evt.which)
     return evt.key;
 }

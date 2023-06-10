1d163d66dff00cffa3d00afe5c5fbbae89446461
jimmy <jmetz2510@gmail.com>
2023-06-09 20:44:28 -0400

    Add multiply, divide, and percent functionality

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ let chosenOperator;
 
 function isNumberKey(evt) {
     let charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode === 190) {
+        console.log(evt.which)
+        return evt.key;
+    }
     if (charCode != 46 && charCode > 31
         && (charCode < 48 || charCode > 57))
         return false;
+    console.log(evt.which)
+    return evt.key;
+}
 
-    console.log(evt)
-    return true;
+function keySelectionHighlight(button) {
+    console.log(button)
+    button.classList.add("highlight")
+    setTimeout(() => {
+        button.classList.remove("highlight")
+    }, 100)
 }
 
 function clearOutputWindow() {
@@ -38,32 +49,57 @@ function storeFirstValue() {
 function add() {
     storeFirstValue();
     clearOutputWindow();
-    chosenOperator = "+"
+    chosenOperator = "+";
 };
 
 function subtract() {
     storeFirstValue();
     clearOutputWindow();
-    chosenOperator = "-"
+    chosenOperator = "-";
+}
+
+function multiply() {
+    storeFirstValue();
+    clearOutputWindow();
+    chosenOperator = "*";
+}
+
+function divide() {
+    storeFirstValue();
+    clearOutputWindow();
+    chosenOperator = "/";
+}
+
+function percent() {
+    storeFirstValue();
+    clearOutputWindow();
+    let result = (+cachedValue) / 100;
+    outputNumberList = [result];
+    outputNumber.innerText = result;
+    clearCachedValue();
 }
 
 function equals() {
     let secondNumber = outputNumber.innerText
     let result
     if (chosenOperator === "+") {
-        result = (+cachedValue) + (+secondNumber)
+        result = (+cachedValue) + (+secondNumber);
     } else if (chosenOperator === "-") {
-        result = (+cachedValue) - (+secondNumber)
+        result = (+cachedValue) - (+secondNumber);
+    } else if (chosenOperator === "*") {
+        result = (+cachedValue) * (+secondNumber);
+    } else if (chosenOperator) {
+        result = (+cachedValue) / (+secondNumber);
     }
-    outputNumberList = [result]
+    outputNumberList = [result];
     outputNumber.innerText = result;
     clearCachedValue();
     return
 }
 
-
 const btns = btn.forEach((e) => {
     e.addEventListener("click", () => {
+        keySelectionHighlight(e)
         let newestNumber = e.innerText;
         const numberTest = Number(newestNumber)
         if (numberTest || newestNumber === ".") {
@@ -78,3 +114,23 @@ const btns = btn.forEach((e) => {
         }
     })
 })
+
+document.addEventListener("keydown", (e) => {
+    let number = isNumberKey(e)
+    console.log(`${e.key} was pressed`)
+    // console.log(number)
+    if (number === "." || number) {
+        if (outputNumberList.includes(".") && number === ".") {
+            return
+        } else {
+            console.log(number)
+            outputNumberList.push(number);
+            outputNumber.innerText = outputNumberList.join('')
+        }
+    } else {
+        return
+    }
+})
+
+document.addEventListener()
+
